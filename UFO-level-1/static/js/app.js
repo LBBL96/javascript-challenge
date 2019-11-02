8fdf8c10b3930087c99190f2fcbb76426dc23663
Leif96 <lynn.leifker@gmail.com>
2019-11-02 16:03:15 -0500

    working table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
+console.log(tableData)
 
-// YOUR CODE HERE!
+
+// // Select the submit button
+// var submit = d3.select("#submit");
+
+// Creating handle variable
+var el = d3.selectAll("tbody")
+    .selectAll("tr")
+    .data(tableData);
+
+
+// Loading table data 
+el.enter()
+    .append("tr")
+    .html(aliens => {
+      return `<td>${aliens.datetime}</td>
+      <td>${aliens.city}</td>
+      <td>${aliens.state}</td>
+      <td>${aliens.country}</td>
+      <td>${aliens.shape}</td>
+      <td>${aliens.durationMinutes}</td>
+      <td>${aliens.comments}</td>`
+    })
+    .append(d => [d.datetime, d.city, d.state, d.country, d.shape, d.durationMinutes, d.comments]);
+
