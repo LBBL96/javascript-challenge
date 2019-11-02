9645c3a6bd69c7cc87eca306b004f13dbdb5545a
Leif96 <lynn.leifker@gmail.com>
2019-11-02 16:42:33 -0500

    corrected

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,10 +9,30 @@ submit.on("click", function(){
   var inputElement = d3.select("#filter-btn");
   var inputValue = inputElement.property("value");
   console.log(inputValue);
-  console.log(tableData)
+  // console.log(tableData)
 
   var filteredData = tableData.filter(aliens => aliens.datetime === inputValue);
-  console.log(filteredData)
+  // console.log(filteredData)
+  var el = d3.selectAll("tbody")
+    .selectAll("tr")
+    .data(tableData);
+
+
+// Loading table data 
+  el.enter()
+    .append("tr")
+    .html(aliens => {
+      return `<td>${aliens.datetime}</td>
+      <td>${aliens.city}</td>
+      <td>${aliens.state}</td>
+      <td>${aliens.country}</td>
+      <td>${aliens.shape}</td>
+      <td>${aliens.durationMinutes}</td>
+      <td>${aliens.comments}</td>`
+    })
+    .append("tr")
+    .data(d => [filteredData.datetime, filteredData.city, filteredData.state, 
+      filteredData.country, filteredData.shape, filteredData.durationMinutes, filteredData.comments]);
 
 });
 
@@ -34,5 +54,6 @@ el.enter()
       <td>${aliens.durationMinutes}</td>
       <td>${aliens.comments}</td>`
     })
-    .append(d => [d.datetime, d.city, d.state, d.country, d.shape, d.durationMinutes, d.comments]);
+    .append("tr")
+    .data(d => [d.datetime, d.city, d.state, d.country, d.shape, d.durationMinutes, d.comments]);
 
